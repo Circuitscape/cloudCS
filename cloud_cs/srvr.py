a157b09fcc6820386d38f0cfaf54df314e2e07ce
tan <tanmaykm@gmail.com>
2013-12-05 18:41:11 +0530

    log window, shutdown functionalities added

--- cloud_cs/srvr.py
+++ cloud_cs/srvr.py
@@ -1,15 +1,69 @@
-import sys, os, webbrowser, json, logging
+import sys, os, webbrowser, json, logging, getpass, tempfile
 from circuitscape.compute import Compute
+from circuitscape import __version__ as cs_version
+from circuitscape import __author__ as cs_author
+from circuitscape import __file__ as cs_pkg_file
 
 import tornado.web
 import tornado.websocket
 import tornado.httpserver
 import tornado.ioloop
 
+SERVER_HOST = "localhost"
+SERVER_PORT = 8080
+SERVER_WS_PATH = r'/websocket'
+SERVER_WS_URL = "ws://" + SERVER_HOST + ':' + str(SERVER_PORT) + SERVER_WS_PATH
+
+logging.basicConfig()
+logger = logging.getLogger('cloudCS')
+logger.setLevel(logging.DEBUG)
+
+def stop_webserver():
+    ioloop = tornado.ioloop.IOLoop.instance()
+    ioloop.add_callback(lambda x: x.stop(), ioloop)
+
+def run_webserver(ws_app, port, start_browser):
+    server = tornado.httpserver.HTTPServer(ws_app)
+    server.listen(port)
+    if start_browser:
+        webbrowser.open('http://localhost:' + str(port) + '/')
+    tornado.ioloop.IOLoop.instance().start()
+    
+
+class WebSocketLogger(logging.Handler):
+    def __init__(self, dest=None):
+        logging.Handler.__init__(self)
+        self.dest = dest
+        self.level = logging.DEBUG
+
+    def flush(self):
+        pass
+
+    def emit(self, record):
+        msg = self.format(record)
+        msg = msg.strip('\r')
+        msg = msg.strip('\n')
+        resp_nv = {
+                   'msg_type': WSMsg.SHOW_LOG,
+                   'data': msg
+        }
+        self.dest.write_message(resp_nv, False)
+        
+
 class WSMsg:
+    RSP_ERROR = -1
+    SHOW_LOG = 0
+    
     REQ_FILE_LIST = 1
     RSP_FILE_LIST = 2
-    RSP_ERROR = -1
+    
+    REQ_LOGOUT = 3
+    RSP_LOGOUT = 4
+    
+    REQ_RUN_VERIFY = 5
+    RSP_RUN_VERIFY = 6
+    
+    
     def __init__(self, msg_type=None, msg_nv=None, msg=None):
         if msg:
             self.nv = json.loads(msg)
@@ -18,8 +72,7 @@ class WSMsg:
                        'msg_type': msg_type,
                        'data': msg_nv
             }
-        print("got WSMsg msg_type: " + str(self.nv['msg_type']) + " type:" + str(type(self.nv['msg_type'])))
-        print("got WSMsg data: " + str(self.nv['data']))
+        logger.debug('received msg_type: %d, data[%s]' % (self.nv['msg_type'], str(self.nv['data'])))
 
     def msg_type(self):
         return self.nv['msg_type']
@@ -44,49 +97,96 @@ class WSMsg:
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        pass
+        logger.debug("websocket connection opened")
+            
+    def on_close(self):
+        logger.debug("websocket connection closed")
 
-    def dump(self, obj):
-        for attr in dir(obj):
-            print "obj.%s = %s" % (attr, getattr(obj, attr))
+#     def dump(self, obj):
+#         for attr in dir(obj):
+#             print "obj.%s = %s" % (attr, getattr(obj, attr))
     
     def on_message(self, message):
         #self.dump(message)
-        print("message=[%s]"%(str(message),))
+        logger.debug("websocket message received [%s]"%(str(message),))
         wsmsg = WSMsg(msg=message)
+        is_shutdown_msg = False
+        response = {}
         try:
             if (wsmsg.msg_type() == WSMsg.REQ_FILE_LIST):
-                filelist = [('..', True)]
-                curdir = wsmsg.data('cwd', os.getcwd());
-                newdir = wsmsg.data('dir')
-                if newdir:
-                    curdir = os.path.normpath(os.path.join(curdir, newdir));
-                for fname in os.listdir(curdir):
-                    full_fname = os.path.join(curdir, fname)
-                    is_dir = os.path.isdir(full_fname)
-                    #print (fname, is_dir)
-                    filelist.append((fname, is_dir))
-                response = {'filelist': filelist, 'dir': curdir}
+                response, is_shutdown_msg = self.handle_file_list(wsmsg)
+            elif (wsmsg.msg_type() == WSMsg.REQ_LOGOUT):
+                response, is_shutdown_msg = self.handle_logout(wsmsg)
+            elif (wsmsg.msg_type() == WSMsg.REQ_RUN_VERIFY):
+                response, is_shutdown_msg = self.handle_run_verify(wsmsg)
                 
             wsmsg.reply(response, self)
         except Exception as e:
-            logging.exception("Exception handling message of type %s"%(wsmsg.msg_type(),), e)
+            logger.exception("Exception handling message of type %s"%(wsmsg.msg_type(),), e)
             wsmsg.error(-1, self)
+            
+        if is_shutdown_msg:
+            stop_webserver()
 
-    def on_close(self):
-        pass
+    def handle_run_verify(self, wsmsg):
+        outdir = None
+        cwd = os.getcwd()
+        try:
+            root_path = os.path.dirname(cs_pkg_file)
+            outdir = tempfile.mkdtemp()
+            if os.path.exists(root_path):
+                root_path = os.path.split(root_path)[0]
+                os.chdir(root_path)     # otherwise we are running inside a packaged folder and resources are availale at cwd
+            wslogger = WebSocketLogger(self)
+            from circuitscape.verify import cs_verifyall
+            testResult = cs_verifyall(out_path=outdir, ext_logger=wslogger)
+            testsPassed = testResult.wasSuccessful()
+        except:
+            testsPassed = False
+        finally:
+            os.chdir(cwd)
+            if None != outdir:
+                for root, dirs, files in os.walk(outdir, topdown=False):
+                    for name in files:
+                        os.remove(os.path.join(root, name))
+                    for name in dirs:
+                        os.rmdir(os.path.join(root, name))
+                os.rmdir(outdir)
+                
+        return ({'complete': True, 'success': testsPassed}, False)
+    
+    def handle_logout(self, wsmsg):
+        return ({}, True)
 
+    def handle_file_list(self, wsmsg):
+        filelist = [('..', True)]
+        curdir = wsmsg.data('cwd', os.getcwd());
+        newdir = wsmsg.data('dir')
+        if newdir:
+            curdir = os.path.normpath(os.path.join(curdir, newdir));
+        for fname in os.listdir(curdir):
+            full_fname = os.path.join(curdir, fname)
+            is_dir = os.path.isdir(full_fname)
+            #print (fname, is_dir)
+            filelist.append((fname, is_dir))
+        return ({'filelist': filelist, 'dir': curdir}, False)
 
 class IndexPageHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("cs.html")
+        kwargs = {
+                  'username': getpass.getuser(),
+                  'version': cs_version,
+                  'author': cs_author,
+                  'ws_url': SERVER_WS_URL,
+        }
+        self.render("cs.html", **kwargs)
 
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', IndexPageHandler),
-            (r'/websocket', WebSocketHandler),
+            (SERVER_WS_PATH, WebSocketHandler),
             (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'templates/static'}),
             (r'/ext/(.*)', tornado.web.StaticFileHandler, {'path': 'ext'})
         ]
@@ -99,9 +199,7 @@ class Application(tornado.web.Application):
 
 
 if __name__ == '__main__':
-    ws_app = Application()
-    server = tornado.httpserver.HTTPServer(ws_app)
-    server.listen(8080)
-    #webbrowser.open('http://localhost:8080/') 
-    tornado.ioloop.IOLoop.instance().start()
-    
+    logger.debug('starting up...')
+    run_webserver(Application(), SERVER_PORT, False)
+
+
--- templates/static/script.js
+++ templates/static/script.js
@@ -1,3 +1,174 @@
-$(document).ready(function(){
-    $('#test_modal').modal('show');
-});
+var cs_ws_url = '';
+
+function do_ws(onopen, onmessage) {
+	ws_conn = new WebSocket(cs_ws_url);
+	ws_conn.onopen = onopen;
+	ws_conn.onmessage = onmessage;
+	return ws_conn;
+}
+
+var file_dlg_result_target = '';
+var file_dlg_conn;
+var file_dlg_cwd = '';
+
+var file_dlg_headers = {
+	'habitat_file_select': "Choose a Habitat Map",
+	'habitat_mask_file_select': "Choose a Habitat Mask",
+	'short_circuit_file_select': "Choose a Short Circuit Map",
+	'focal_nodes_file_select': "Choose a Focal Node Map",
+	'incl_excl_file_select': "Choose an Include/Exclude Nodes File",
+	'src_strength_file_select': "Choose a file with source strengths",
+	'current_sources_file_select': "Choose a file with current sources",
+	'ground_points_file_select': "Choose a file with ground points",
+};
+
+function file_dlg_list(dir) {
+	data = {};
+	if(file_dlg_cwd.length > 0) {
+		data['cwd'] = file_dlg_cwd;
+	}
+	if(dir) {
+		data['dir'] = dir;
+	}
+	file_dlg_conn.send(JSON.stringify({
+		'msg_type': 1,
+		'data': data
+	}));				
+};
+
+function file_dlg_init(target_id) {
+	file_dlg_result_target = target_id.substring(0, target_id.lastIndexOf('_'));
+	$('#'+file_dlg_result_target).val('');
+	$('#filedlg_div_title').html(file_dlg_headers[target_id]);
+	$('#filedlg_div').modal('show');
+	$('#filedlg_div').on('hidden.bs.modal', function() {
+		if(file_dlg_conn) {
+			file_dlg_conn.close();						
+		}
+	});
+
+	file_dlg_conn = do_ws(file_dlg_list, 
+		function(evt) {
+			resp = JSON.parse(evt.data);
+			if (resp.msg_type == 2) {
+				file_dlg_cwd = resp.data.dir;
+				$('#filedlg_div_dir').html(file_dlg_cwd);
+				files_html = "";
+				for(idx=0; idx < resp.data.filelist.length; idx++) {
+					filename = resp.data.filelist[idx][0];
+					is_dir = resp.data.filelist[idx][1];
+					files_html += ("<li class=\"" + (is_dir ? "filedlg_dir" : "filedlg_file") + "\">" + filename + "</li>"); 
+				}
+				$('#filedlg_div_files').html(files_html);
+				$('.filedlg_dir').click(function(e){
+					file_dlg_list($(this).html());
+				});
+				$('.filedlg_file').click(function(e){
+					sel_file = $(this).html();
+					sel_file = file_dlg_cwd + "/" + sel_file;
+					$('#'+file_dlg_result_target).val(sel_file);
+					$('#filedlg_div').modal('hide');
+				});
+			}
+		});
+};
+
+function select_input_format(selVal) {
+	if(selVal == 'raster') {
+		$('.on_raster').show();
+		$('.on_network').hide();
+	}
+	else {
+		$('.on_raster').hide();
+		$('.on_network').show();
+	}
+};
+
+function select_modeling_mode(selVal) {
+	$('.on_modeling_mode').hide();
+	$('.on_modeling_mode').filter($('.on_'+selVal)).show();
+};
+
+function logoff() {
+	do_ws(function() {
+			ws_conn.send(JSON.stringify({
+				'msg_type': 3,
+				'data': ""
+			}));
+		},
+		function(evt) {
+			resp = JSON.parse(evt.data);
+			if (resp.msg_type == 4) {
+				ws_conn.close();
+				$('body').html('<p></p>');
+			}
+		});
+};
+
+function run_verify() {
+	do_ws(function() {
+			$('#verify_div').modal({ backdrop: 'static', keyboard: false });
+			$('#verify_div_msg').val('');
+			$('#verify_div').modal('show');
+			$('#verify_div_close').attr('disabled', 'disabled');
+			ws_conn.send(JSON.stringify({
+				'msg_type': 5,
+				'data': ""
+			}));
+		},
+		function(evt) {
+			resp = JSON.parse(evt.data);
+			if (resp.msg_type == 6) {
+				alert("complete=" + resp.data.complete);
+				alert("success=" + resp.data.success);
+				ws_conn.close();
+				$('#verify_div_close').removeAttr('disabled');
+			}
+			else if(resp.msg_type == 0) {
+				$('#verify_div_msg').val(resp.data + '\n' + $('#verify_div_msg').val());
+			}
+		});	
+};
+
+function init_circuitscape(ws_url) {
+	cs_ws_url = ws_url;
+	$('input[id=habitat_file_hidden]').change(function() {
+		$('#habitat_file').val($(this).val());
+	});
+	$('#input_tabs').tab();
+	$('#input_tabs a').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+	$('.server_file_dlg').click(function(e){
+		file_dlg_init(e.target.id);
+	});
+	
+	$('#num_parallel_procs').spinedit({
+		minimum: 1,
+		maximum: 10,
+		step: 1,
+		value: 1,
+		numberOfDecimals: 0
+	});
+	
+	select_modeling_mode('pairwise');
+	$('#modeling_mode input:radio').change(function() {
+		var selVal = $("#modeling_mode input:radio:checked").val();
+		select_modeling_mode(selVal);
+	});
+	
+	select_input_format('raster');
+	$('#input_format input:radio').change(function() {
+		var selVal = $("#input_format input:radio:checked").val();
+		select_input_format(selVal);
+	});
+	
+	$('#shutdown').click(function(e){
+		$('#logout_div').modal('show');	
+	});
+	
+	$('#menu_verify').click(function(e){
+		run_verify();
+	});
+};
