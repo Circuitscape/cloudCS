2c5afeca759fcee6598e22b0b8b89c539966e79b
tan <tanmaykm@gmail.com>
2014-02-24 23:07:20 +0530

    auto refresh users roles configuration

--- cloud_cs/cfg.py
+++ cloud_cs/cfg.py
@@ -1,3 +1,4 @@
+import time
 
 class ServerConfig:
     SERVER_WS_PATH = r'/websocket'
@@ -16,23 +17,35 @@ class ServerConfig:
         self.listen_ip = self.cfg_get("listen_ip", str, "0.0.0.0")
         self.headless = self.cfg_get("headless", bool, False)
         
-        self.allowed_users = self.cfg_get("allowed_users", str)
-        if self.allowed_users != None:
-            self.allowed_users = [line.strip() for line in open(self.allowed_users, 'r')]
-            
-        self.user_roles = self.cfg_get("user_roles", str)
-        if self.user_roles != None:
-            user_roles = {}
-            for line in open(self.user_roles, 'r'):
-                uid, role = line.strip().split()
-                user_roles[uid] = role
-            self.user_roles = user_roles
+        self.allowed_users_file = self.cfg_get("allowed_users", str)
+        self.user_roles_file = self.cfg_get("user_roles", str)
+        self.last_refresh_time = 0
+        self.refresh_user_list()
             
         self.http_url = "http://" + self.host + ':' + str(self.port) + '/'
         self.ws_url = "http://" + self.host + ':' + str(self.port) + ServerConfig.SERVER_WS_PATH
         self.storage_auth_redirect_uri = "http://" + self.host + ':' + str(self.port) + ServerConfig.SERVER_STORAGE_AUTH_PATH
     
+    def refresh_user_list(self):
+        now = time.time()
+        if (now - self.last_refresh_time) < 60*10:
+            return
+        self.last_refresh_time = now
+        if self.allowed_users_file != None:
+            with open(self.allowed_users_file, 'r') as f:
+                self.allowed_users = [line.strip() for line in f]
+            
+        if self.user_roles_file != None:
+            user_roles = {}
+            with open(self.user_roles_file, 'r') as f:
+                for line in f:
+                    uid, role = line.strip().split()
+                    user_roles[uid] = role
+                self.user_roles = user_roles
+        
+    
     def is_user_allowed(self, user):
+        self.refresh_user_list()
         if self.allowed_users == None:
             return True
         return (user in self.allowed_users)
